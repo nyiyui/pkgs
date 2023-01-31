a0311b510a71e0a8024d59e98595dae7b9166fdf
nyiyui <+@nyiyui.ca>
2023-01-30 22:44:41 -0500

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Toukouhou (透虹包)
+
+Collections of random Nix flakes for random packages.
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in rec {
+    packages.udp2raw = pkgs.stdenv.mkDerivation {
+      name = "udp2raw";
+      src = pkgs.fetchFromGitHub {
+        owner = "wangyu-";
+        repo = "udp2raw";
+        rev = "cc6ea766c495cf4c69d1c7485728ba022b0f19de";
+        sha256 = "1pq6k5dwlv85g3fwizsq45fkgs8mg1ix4andnxrp4z2ibmycwi2f";
+      };
+      buildPhase = ''
+        make dynamic
+      '';
+      postFixup = ''
+        wrapProgram $out/bin/udp2raw \
+          --set PATH ${pkgs.lib.makeBinPath [ pkgs.iptables ]}
+      '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp udp2raw_dynamic $out/bin/udp2raw
+      '';
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        pkgs.makeWrapper
+      ];
+    };
+  });
+}
