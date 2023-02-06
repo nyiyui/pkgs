45f5b474d291dff6a621e83664dc8be9af2cd6d6
nyiyui <45807097+nyiyui@users.noreply.github.com>
2023-02-06 12:50:00 -0500

    udp2raw: parametrize

--- flake.nix
+++ flake.nix
@@ -1,4 +1,7 @@
-{
+let
+  rev = "cc6ea766c495cf4c69d1c7485728ba022b0f19de";
+  sha256 = "1pq6k5dwlv85g3fwizsq45fkgs8mg1ix4andnxrp4z2ibmycwi2f";
+in {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
@@ -13,8 +16,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "wangyu-";
         repo = "udp2raw";
-        rev = "cc6ea766c495cf4c69d1c7485728ba022b0f19de";
-        sha256 = "1pq6k5dwlv85g3fwizsq45fkgs8mg1ix4andnxrp4z2ibmycwi2f";
+        inherit rev;
+        inherit sha256;
       };
       buildPhase = ''
         make dynamic
