5c1980f26af6287bac60e4da18664f067dcf56ad
nyiyui <45807097+nyiyui@users.noreply.github.com>
2023-02-06 17:40:36 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,4 @@
-let
-  rev = "cc6ea766c495cf4c69d1c7485728ba022b0f19de";
-  sha256 = "1pq6k5dwlv85g3fwizsq45fkgs8mg1ix4andnxrp4z2ibmycwi2f";
-in {
+{
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
@@ -11,7 +8,10 @@ in {
   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
     pkgs = nixpkgs.legacyPackages.${system};
   in rec {
-    packages.udp2raw = pkgs.stdenv.mkDerivation {
+    packages.udp2raw = let
+      rev = "cc6ea766c495cf4c69d1c7485728ba022b0f19de";
+      sha256 = "1pq6k5dwlv85g3fwizsq45fkgs8mg1ix4andnxrp4z2ibmycwi2f";
+    in pkgs.stdenv.mkDerivation {
       name = "udp2raw";
       src = pkgs.fetchFromGitHub {
         owner = "wangyu-";
